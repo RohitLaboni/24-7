22b29a0adb8adf12e4c85366574c6fc96cddf05d
RohitLaboni <151800110+RohitLaboni@users.noreply.github.com>
2026-05-03 06:22:14 +0530

    Update Telegram bot token and chat ID

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const TelegramBot = require('node-telegram-bot-api');
 const cron = require('node-cron');
 
 // Your Telegram bot token
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '8665621150:AAE1AYh-ZNs0md9mvB3X8RxgUGpZbeef9XE';
 const bot = new TelegramBot(token, {polling: true});
 
 // Schedule a message every 1.5 hours
 cron.schedule('0 */90 * * *', () => {
-    bot.sendMessage('YOUR_CHAT_ID', 'This is an automated high-send message!');
+    bot.sendMessage('-1003718062610', 'This is an automated high-send message!');
 });
